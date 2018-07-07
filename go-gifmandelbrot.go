52dab25cbe121897d35f55a32715f1b705c7ad0d
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-07-07 16:25:11 +0900

    const, methodの切り出し

--- go-gifmandelbrot.go
+++ go-gifmandelbrot.go
@@ -11,29 +11,17 @@ import (
 
 var palette = []color.Color{color.White, color.Black}
 
+const (
+	xmin, ymin, xmax, ymax = -2, -2, +2, +2
+	width, height          = 1024, 1024
+	nframes                = 200 // アニメーションフレーム数
+	delay                  = 8   // 10ms単位でのフレーム間の遅延
+)
+
 func main() {
-	const (
-		xmin, ymin, xmax, ymax = -2, -2, +2, +2
-		width, height          = 1024, 1024
-		nframes                = 200 // アニメーションフレーム数
-		delay                  = 8   // 10ms単位でのフレーム間の遅延
-	)
 	anim := gif.GIF{LoopCount: nframes}
 	for pow := 1.0; pow < 10.0; pow += 0.02 {
-		rect := image.Rect(0, 0, width, height)
-		img := image.NewPaletted(rect, palette)
-
-		for py := 0; py < height; py++ {
-			y := float64(py)/height*(ymax-ymin) + ymin
-			for px := 0; px < width; px++ {
-				x := float64(px)/width*(xmax-xmin) + xmin
-				z := complex(x, y)
-				// 画像の点(px, py)は複素数値zを表している。
-				img.Set(px, py, mandelbrot(z, pow))
-			}
-		}
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
+		draw(&anim, pow)
 	}
 	// gif に保存する
 	f, _ := os.OpenFile("out.gif", os.O_WRONLY|os.O_CREATE, 0600)
@@ -52,4 +40,21 @@ func mandelbrot(z complex128, pow float64) color.Color {
 		}
 	}
 	return color.Black
+}
+
+func draw(anim *gif.GIF, pow float64) {
+	rect := image.Rect(0, 0, width, height)
+	img := image.NewPaletted(rect, palette)
+
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// 画像の点(px, py)は複素数値zを表している。
+			img.Set(px, py, mandelbrot(z, pow))
+		}
+	}
+	anim.Delay = append(anim.Delay, delay)
+	anim.Image = append(anim.Image, img)
 }
